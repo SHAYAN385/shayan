0884b6e7a4a314f164d220ffba188095a1bcaec9
unknown <shayan.baig385@gmail.com>
2022-06-30 17:52:57 +0500

    work

---
+++ app.js
@@ -0,0 +1,114 @@
+// chapter 01
+// alert("Error! Please enter a valid password");
+
+// alert("Welcome to JS Land..Happy Coding!");
+
+// alert("Welcome to JS Land...");
+// alert("Prevent this page from creating addition dialogs");
+
+// console.log("Hello... I can run JS through my web browser");
+
+// Chapter 2
+// var username = "username."
+// var myname ="Mirza Shayan Baig"
+
+// var message ="Hello World"
+// alert(message)
+
+// alert("Jhon Doe");
+// alert("15 years old");
+// alert("Certified Mobile Application Development");
+
+// alert("PIZZA  \nPIZZ \nPIZ \nPI \nP");
+
+// var email ="My mail address is shyaanbaig@yahoo.com"; 
+// alert(email);
+// var Book ="I am trying to learn from the Book A smarter way to learn JavaScript"
+// alert(Book)
+// document.write("Yah! I can write HTML content throug JavaScript")
+
+// Chapter 3
+// alert("I am 15 years old");
+
+// alert("You have visited this site 1 time")
+// alert("You have visited this site 2 time")
+// alert("You have visited this site 3 time")
+// alert("You have visited this site 4 time")
+// alert("You have visited this site 5 time")
+// alert("You have visited this site 6 time")
+// alert("You have visited this site 7 time")
+// alert("You have visited this site 8 time")
+// alert("You have visited this site 9 time")
+// alert("You have visited this site 10 time")
+// alert("You have visited this site 11 time")
+// alert("You have visited this site 12 time")
+// alert("You have visited this site 13 time")
+// alert("You have visited this site 14 time")
+// alert("You have visited this site 15 time")
+
+// document.write("My birth year is 1990 <br/>Data type of my declared variable is number ");
+
+
+
+// document.write("<b>jhone doe</b> ordered <b>5 T-shirt</b>(s) on ABC Clothing store");
+
+// chapter 04
+// 3 variables
+// var shyaan = "dhjdhjd";
+// var baig = "sjhxk";
+// var mirza = "sjkhsjkhs";
+
+// legal & illegal variable
+
+// var $my = "gfsg";
+// var _name ="gsfg";
+// var shayam = "gfsg";
+// var shay123 = "ddsgdf";
+// var _name 123 ="gfg";
+
+// var @abc = "ghg";
+// var 123 = "sjhsj";
+// var -abc = "hsuhs";
+// var &456 = "sjhjs";
+// var 123shyaan = "sgxhs";
+
+// document.write("<h1>Rules for naming JS variables</h1>");
+// document.write ("Variable names can only contain number,  $, and _ . For example: $my, _1stVariable <br/>Variable must begin with a letter, $ or _ .For example: $name,  _name or name.<br/>Variable names are case sensitive <br/> Variable names should not be JS keywords")
+
+// chapter 05
+
+// document.write("Sum of 3 and 5 is 8");
+
+// document.write("subtraction of 5 and 3 is 2");
+
+// document.write("multiplication  of 5 by 5 is 25");
+
+// document.write("divide of 8 and 2 is 16");
+
+// document.write("<h1>After variable declaration is undefined <br/> Initial value: 5 <br/>Value after increment is: 6 <br/> Value after addition is: 13 <br/>Value after decrement is: 12  <br/> The remainder is : 0 </h1>");
+
+// document.write("<h1>Total cost to buy 5tickets to a movie is 3000PKR</h1>");
+//  document.write("<h1>Table of 4 <br/> 4*1=4 <br/> 4*2=8 <br/> 4*3=12 <br/> 4*4=16 <br/> 4*5=20 <br/> 4*6=24 <br/> 4*7=28 <br/> 4*8=32 <br/> 4*9=36 <br/> 4*10=40</h1>");
+
+// document.write("<h1>25'C is 77'F <br/> 70'F is 21.111111111111111'C</h1>");
+
+// document.write("<h1>Shopping Cart <br/>   <br/>  Price of item 1 is 650 <br/>  Ordered quantity of item 1 is 3<br/>  Price of item 2 is 100 <br/>  Price of item 2 is 7 <br/>  Shipping charges 100  <br/>    <br/> Total cost of your order is 2750</h1>")
+
+// document.write("<h1>Marks Sheet <br/>   <br/>Total marks:980 <br/>Marks Obtained:804 <br/>Percentage:82.0408163265306%</h1>")
+
+// document.write("<h1>Currency in PKR <br/>  <br/>   <br/>  Total currency in PKR:1748 </h1>");
+
+// document.write("<h1>Age Calculator<br/>  <br/>   <br/>Current Year:2016 <br/> Birth Year:1992 <br/> Your Age is:24 </h1>");
+
+// document.write("<h1>The Geometrizer<br/>  <br/>   <br/>Radius of a circle:20 <br/>The circumference is:125.679999999<br/>The area is:1256.8</h1>");
+// document.write("<h1>The Lifetime Supply Calculator<br/>  <br/>   <br/>Favourite Snack:Chocolate chip<br/>Current age:15<br/>Estimated Maximum Age:65 <br/> Amount of snacks per day:3 <br/> You will need 150 chocolate chip to last you until the ripe old age of 65 </h1>");
+// chapter 06
+// document.write("Results: <br/> The value of a is 10 <br/> ........................ <br/>    <br/> The value of ++a is:11 <br/> Now the value of a is:11 <br/>    <br/> The value of a++ is:11 <br/> Now the value of a is:12 <br/>    <br/> The value of --a is:11 <br/> Now the value of a is:11 <br/>   <br/>  The value of --a is:11 <br/> Now the value of a is:10");
+
+var a =2;
+var b =1;
+
+// document.write(--a);
+// document.write(--a - --b);
+// document.write(--a - --b + ++b);
+// document.write(--a - --b + ++b + b--);
